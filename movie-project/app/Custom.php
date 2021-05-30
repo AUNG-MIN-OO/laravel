2c45398b3efbe584f7efd170b0c20c086bf966f9
Aung Min Oo <aungminoo1314@gmail.com>
2021-05-30 23:59:27 +0900

    UI frame for movie-project

---
+++ movie-project/app/Custom.php
@@ -0,0 +1,22 @@
+<?php
+
+
+namespace App;
+
+
+class Custom
+{
+    public static $info =  [
+
+        "name" => "Dashboard Frame",
+        "short_name" => "Frame",
+        "type" => "dashobard",
+        "phone" => "",
+        "address" =>"",
+        "meta-img" => "f/img/meta.jpg",
+        "mms-logo" => "dashboard/images/mms-logo.jpg",
+        "c-logo" => "dashboard/images/logo.jpg",
+        "main_css" => "dashboard/css/bootstrap.min.css",
+    ];
+
+}
